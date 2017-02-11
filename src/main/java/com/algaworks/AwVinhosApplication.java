0f24cb52ff6fa6df93d55557b6a4e6f6f6d4a5d6
Marlon Klagenberg <marlon.klc@gmail.com>
2017-02-11 03:27:27 -0200

    initializer spring boot

---
+++ src/main/java/com/algaworks/AwVinhosApplication.java
@@ -0,0 +1,12 @@
+package com.algaworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwVinhosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwVinhosApplication.class, args);
+	}
+}
