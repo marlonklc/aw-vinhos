67b51ebe151de0df5fb2c5230f182559b88b4574
Marlon Klagenberg <marlon.klc@gmail.com>
2017-06-20 00:47:40 -0300

    Implementado modal para exclusão de vinhos

--- src/main/java/com/algaworks/vinhos/controller/VinhosController.java
+++ src/main/java/com/algaworks/vinhos/controller/VinhosController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -28,6 +29,7 @@ public class VinhosController {
     @GetMapping("/novo")
     public ModelAndView novo(Vinho vinho) {
         ModelAndView mv = new ModelAndView("vinhos/cadastro-vinho");
+        mv.addObject(vinho);
         mv.addObject("tipos", TipoVinho.values());
         return mv;
     }
@@ -50,5 +52,11 @@ public class VinhosController {
     	        Optional.ofNullable(vinhoFilter.getNome()).orElse("%")));
 
     	return mv;
+    }
+
+    @GetMapping("/{codigo}")
+    public ModelAndView editar(@PathVariable Long codigo) {
+        Vinho vinho = repository.findOne(codigo);
+        return novo(vinho);
     }
 }
